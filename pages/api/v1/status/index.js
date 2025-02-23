a268ae832df39a7c08ddc08e00f7994e295822e5
Juan Veronez <juan.monteirov@gmail.com>
2025-02-23 01:59:35 -0300

    Creates `infra/database.js` and create `.env` file, but ignore it

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(_, response) {
+import { query } from "../../../../infra/database";
+
+async function status(_, response) {
+  const result = await query("SELECT 1 + 1 AS sum;");
+  console.log(result.rows);
   response.status(200).json({ mensagem: "A aplicação parece saudável!" });
 }
 
