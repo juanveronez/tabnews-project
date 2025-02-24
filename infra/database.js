61dd20379c94bae472834115b5d4c252dcc5d6bc
Juan Veronez <juan.monteirov@gmail.com>
2025-02-24 09:07:56 -0300

    make `infra/database` more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ export async function query(queryObject, queryValues) {
     database: process.env.POSTGRES_DB,
   });
 
+  console.log("Credenciais Postgres: ", {
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+  });
+
   try {
     await client.connect();
 
@@ -17,6 +25,7 @@ export async function query(queryObject, queryValues) {
     return result;
   } catch (error) {
     console.error(error);
+    throw error;
   } finally {
     await client.end();
   }
