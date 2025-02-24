e128f34237d2885436b9284ebd0bb26af3a8a6a7
Juan Veronez <juan.monteirov@gmail.com>
2025-02-24 10:58:18 -0300

    Update ssl condition to not development

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ export async function query(queryObject, queryValues) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "production",
+    ssl: process.env.NODE_ENV !== "development",
   });
 
   try {
