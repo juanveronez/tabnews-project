02abd581d05f7e01f1f085d1513a3e61ad6928fa
Juan Veronez <juan.monteirov@gmail.com>
2025-02-24 10:26:25 -0300

    Update `infra/database` to suport ssl on production

--- infra/database.js
+++ infra/database.js
@@ -7,14 +7,7 @@ export async function query(queryObject, queryValues) {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
-  });
-
-  console.log("Credenciais Postgres: ", {
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV === "production",
   });
 
   try {
